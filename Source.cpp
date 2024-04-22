34c72921c83f4a8c4e4b7b437888292b30120264
Atharva Keny <145745120+atharva-keny-0812@users.noreply.github.com>
2024-04-22 20:57:27 +0530

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -25,13 +25,31 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h> 
 
+using namespace std;
+
+// Camera parameters
+float cameraSpeed = 0.05f;
+float cameraYaw = -90.0f;
+float cameraPitch = 0.0f;
+float cameraFoV = 45.0f;
+GLfloat cameraPosition[3] = { 0.0f, 0.0f, 3.0f };
+GLfloat cameraFront[3] = { 0.0f, 0.0f, -1.0f };
+GLfloat cameraUp[3] = { 0.0f, 1.0f, 0.0f };
+
+// Mouse movement variables
+int lastX = 400, lastY = 300;
+bool firstMouse = true;
+
 // Globals.
-static std::vector<float> verticesVector; // Vector to read in vertex x, y and z values fromt the OBJ file.
-static std::vector<int> facesVector; // Vector to read in face vertex indices from the OBJ file.
+vector<float> verticesVector; // Vector to read in vertex x, y and z values fromt the OBJ file.
+vector<int> facesVector; // Vector to read in face vertex indices from the OBJ file.
 static float* vertices = NULL;  // Vertex array of the object x, y, z values.
 static int* faces = NULL; // Face (triangle) vertex indices.
 static int numIndices; // Number of face vertex indices.
 static float Xangle = 0.0, Yangle = 0.0, Zangle = 0.0; // Angles to rotate the object.
+vector<float*> allvertices;
+vector<int> allnumIndices;
+vector<int*> allfaces;
 
 using namespace std;
 
@@ -173,6 +191,30 @@ void loadOBJ(std::string fileName)
 	inFile.close();
 }
 
+void appendOBJ(string filename)
+{
+	// Read the external OBJ file into the internal vertex and face vectors.
+	loadOBJ(filename);
+
+	// Size the vertex array and copy into it x, y, z values from the vertex vector.
+	vertices = new float[verticesVector.size()];
+	for (int i = 0; i < verticesVector.size(); i++) vertices[i] = verticesVector[i];
+
+	// Size the faces array and copy into it face index values from the face vector.
+	faces = new int[facesVector.size()];
+	for (int i = 0; i < facesVector.size(); i++) faces[i] = facesVector[i];
+	numIndices = facesVector.size();
+	cout << numIndices;
+
+	facesVector.clear();
+	verticesVector.clear();
+
+	//glVertexPointer(3, GL_FLOAT, 0, vertices);
+
+	allvertices.push_back(vertices);
+	allfaces.push_back(faces);
+	allnumIndices.push_back(numIndices);
+}
 // Initialization routine.
 void setup(void)
 {
@@ -180,6 +222,26 @@ void setup(void)
 
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 
+	appendOBJ("walls.obj");
+	appendOBJ("Windowtable.obj");
+	appendOBJ("Door.obj");
+	appendOBJ("Doorknob.obj");
+	appendOBJ("floor.obj");
+	appendOBJ("Logo.obj");
+
+	appendOBJ("Barchairs.obj");
+	appendOBJ("chairjoints.obj");
+	appendOBJ("chairlegs.obj");
+	appendOBJ("chairseats.obj");
+	appendOBJ("Counter.obj");
+	appendOBJ("cupsontables.obj");
+
+	appendOBJ("Diningtable.obj");
+	appendOBJ("seatrest.obj");
+	appendOBJ("Teaincup.obj");
+	cout << "Size:" << allnumIndices.size();
+
+	/*
 	// Read the external OBJ file into the internal vertex and face vectors.
 	loadOBJ("baguette.obj");
 
@@ -192,28 +254,108 @@ void setup(void)
 	for (int i = 0; i < facesVector.size(); i++) faces[i] = facesVector[i];
 	numIndices = facesVector.size();
 
+	allvertices.push_back(vertices);
+	allfaces.push_back(faces);
+	allnumIndices.push_back(numIndices);
+
 	glVertexPointer(3, GL_FLOAT, 0, vertices);
+	*/
+}
+
+void drawOBJ(int i, float r, float g, float b)
+{
+	glPushMatrix();
+	glColor3f(r, g, b);
+	float matSpec[] = { 1.0, 1.0, 1.0, 1.0 };
+	float matShine[] = { 50.0 };
+	float matAmbAndDif0[] = { 0.0, 0.8, 0.0, 1.0 };
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, matAmbAndDif0);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, matSpec);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, matShine);
+	glVertexPointer(3, GL_FLOAT, 0, allvertices[i]);
+	glDrawElements(GL_TRIANGLES, allnumIndices[i], GL_UNSIGNED_INT, allfaces[i]);
+	//glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, faces);
+	glPopMatrix();
+
+}
+
+void mouse_callback(int xpos, int ypos) {
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates range from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	cameraYaw += xoffset;
+	cameraPitch += yoffset;
+
+	// Prevent camera from flipping over
+	if (cameraPitch > 89.0f) cameraPitch = 89.0f;
+	if (cameraPitch < -89.0f) cameraPitch = -89.0f;
+
+	GLfloat front[3];
+	front[0] = cos(cameraYaw * M_PI / 180) * cos(cameraPitch * M_PI / 180);
+	front[1] = sin(cameraPitch * M_PI / 180);
+	front[2] = sin(cameraYaw * M_PI / 180) * cos(cameraPitch * M_PI / 180);
+	for (int i = 0; i < 3; ++i)
+		cameraFront[i] = front[i];
 }
 
 // Drawing routine.
 void drawScene(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
 	eye(R, theta, phi);
-	gluLookAt(0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	//gluPerspective(cameraFoV, 800.0f / 600.0f, 0.1f, 100.0f);
+	//gluLookAt(cameraPosition[0], cameraPosition[1], cameraPosition[2], cameraPosition[0] + cameraFront[0], cameraPosition[1] + cameraFront[1], cameraPosition[2] + cameraFront[2], cameraUp[0], cameraUp[1], cameraUp[2]);
+	//gluLookAt(0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	glColor3f(0.0, 0.0, 0.0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
 
 	// Rotate scene.
 	glRotatef(Zangle, 0.0, 0.0, 1.0);
 	glRotatef(Yangle, 0.0, 1.0, 0.0);
 	glRotatef(Xangle, 1.0, 0.0, 0.0);
 
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
+
 	// Draw the object mesh.
-	glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, faces);
+	drawOBJ(0, 0.982, 0.591, 1);		//Walls
+	drawOBJ(1, 0.8, 0.638, 0.423);		//WindowTable
+	drawOBJ(2, 0.178, 0.034, 0.0);		//Door
+	drawOBJ(3, 0.8, 0.483, 0.165);		//DoorKnob
+	drawOBJ(4, 0.791, 0.305, 0.098);	//Floor
+	drawOBJ(5, 0.411, 0.117, 0.065);	//Logo
+
+	drawOBJ(6, 0.799, 0.352, 0.12);     //Barchairs
+	drawOBJ(7, 1.0, 1.0, 1.0);          //chairjoints
+	drawOBJ(8, 0.163, 0.076, 0.029);    //chairlegs
+	drawOBJ(9, 0.799, 0.352, 0.12);     //chairseats
+	drawOBJ(10, 0.799, 0.647, 0.315);   //Counter
+	drawOBJ(11, 0.5, 0.5, 0.5);         //cupsontables
+
+	drawOBJ(12, 0.799, 0.647, 0.315);   //Dining Table
+	drawOBJ(13, 0.799, 0.647, 0.315);   //Seat Rest
+	drawOBJ(14, 0.163, 0.063, 0.017);   //Tea in cup
+
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+	glutPostRedisplay();
+	glFlush();
 
 	glutSwapBuffers();
 }
@@ -230,6 +372,20 @@ void resize(int w, int h)
 
 // Keyboard input processing routine.
 
+// Function to handle keyboard input
+
+// Keyboard input callback
+/*
+void keyInput(unsigned char key, int x, int y)
+{
+	if (key == 'w')
+		for (int i = 0; i < 3; ++i)
+			cameraPosition[i] += cameraSpeed * cameraFront[i];
+	if (key == 's')
+		for (int i = 0; i < 3; ++i)
+			cameraPosition[i] -= cameraSpeed * cameraFront[i];
+}
+*/
 // Function to handle keyboard input
 void keyInput(unsigned char key, int x, int y)
 {
@@ -266,6 +422,7 @@ void keyInput(unsigned char key, int x, int y)
 		break;
 	}
 }
+
 /*
 void keyInput(unsigned char key, int x, int y)
 {
@@ -328,14 +485,17 @@ int main(int argc, char** argv)
 
 	glutInitContextVersion(4, 3);
 	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_RGBA);
 
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("OBJmodelViewer.cpp");
 	glutDisplayFunc(drawScene);
+	//glutPassiveMotionFunc(mouse_callback);
 	glutReshapeFunc(resize);
 	glutKeyboardFunc(keyInput);
+
+	// Enable depth testing
 
 	glewExperimental = GL_TRUE;
 	glewInit();

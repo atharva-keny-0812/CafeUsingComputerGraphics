2dd764aea66092af03c98c2de1228ffb5ce9c686
Atharva Keny <145745120+atharva-keny-0812@users.noreply.github.com>
2024-04-22 21:41:07 +0530

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////
-// OBJmodelViewer.cpp
-//
-// An object defined in an external Wavefront OBJ file is loaded and displayed. 
-// Only vertex and face lines are read. All other lines are ignored. Vertex lines 
-// are assumed to have only x, y and z coordinate values. The (optional) w value 
-// is ignored if present. Within a face line only vertex indices are read. Texture 
-// and normal indices are allowed but ignored. Face lines can have more than three 
-// vertices. If a face line has more than three vertices the output is a fan 
-// triangulation. Therefore, the mesh generated consists of only triangles.
-//
-// Interaction:
-// Press x, X, y, Y, z, Z to turn the object.
-//
-// Sumanta Guha.
-//////////////////////////////////////////////////////////////////////////////////
 #define _USE_MATH_DEFINES
 
 #include <iostream>
@@ -27,26 +11,12 @@
 
 using namespace std;
 
-// Camera parameters
-float cameraSpeed = 0.05f;
-float cameraYaw = -90.0f;
-float cameraPitch = 0.0f;
-float cameraFoV = 45.0f;
-GLfloat cameraPosition[3] = { 0.0f, 0.0f, 3.0f };
-GLfloat cameraFront[3] = { 0.0f, 0.0f, -1.0f };
-GLfloat cameraUp[3] = { 0.0f, 1.0f, 0.0f };
-
-// Mouse movement variables
-int lastX = 400, lastY = 300;
-bool firstMouse = true;
-
 // Globals.
 vector<float> verticesVector; // Vector to read in vertex x, y and z values fromt the OBJ file.
 vector<int> facesVector; // Vector to read in face vertex indices from the OBJ file.
 static float* vertices = NULL;  // Vertex array of the object x, y, z values.
 static int* faces = NULL; // Face (triangle) vertex indices.
 static int numIndices; // Number of face vertex indices.
-static float Xangle = 0.0, Yangle = 0.0, Zangle = 0.0; // Angles to rotate the object.
 vector<float*> allvertices;
 vector<int> allnumIndices;
 vector<int*> allfaces;
@@ -83,9 +53,9 @@ void eye(float R, float theta, float phi)
 	gluLookAt(x, y, z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
-void loadOBJ(std::string fileName)
+void loadOBJ(string fileName)
 {
-	std::string line;
+	string line;
 	int count, vertexIndex1, vertexIndex2, vertexIndex3;
 	float coordinateValue;
 	char currentCharacter, previousCharacter;
@@ -239,75 +209,16 @@ void setup(void)
 	appendOBJ("Diningtable.obj");
 	appendOBJ("seatrest.obj");
 	appendOBJ("Teaincup.obj");
-	cout << "Size:" << allnumIndices.size();
-
-	/*
-	// Read the external OBJ file into the internal vertex and face vectors.
-	loadOBJ("baguette.obj");
-
-	// Size the vertex array and copy into it x, y, z values from the vertex vector.
-	vertices = new float[verticesVector.size()];
-	for (int i = 0; i < verticesVector.size(); i++) vertices[i] = verticesVector[i];
-
-	// Size the faces array and copy into it face index values from the face vector.
-	faces = new int[facesVector.size()];
-	for (int i = 0; i < facesVector.size(); i++) faces[i] = facesVector[i];
-	numIndices = facesVector.size();
-
-	allvertices.push_back(vertices);
-	allfaces.push_back(faces);
-	allnumIndices.push_back(numIndices);
-
-	glVertexPointer(3, GL_FLOAT, 0, vertices);
-	*/
 }
 
 void drawOBJ(int i, float r, float g, float b)
 {
 	glPushMatrix();
 	glColor3f(r, g, b);
-	float matSpec[] = { 1.0, 1.0, 1.0, 1.0 };
-	float matShine[] = { 50.0 };
-	float matAmbAndDif0[] = { 0.0, 0.8, 0.0, 1.0 };
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, matAmbAndDif0);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, matSpec);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, matShine);
 	glVertexPointer(3, GL_FLOAT, 0, allvertices[i]);
 	glDrawElements(GL_TRIANGLES, allnumIndices[i], GL_UNSIGNED_INT, allfaces[i]);
-	//glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, faces);
 	glPopMatrix();
 
-}
-
-void mouse_callback(int xpos, int ypos) {
-	if (firstMouse) {
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates range from bottom to top
-	lastX = xpos;
-	lastY = ypos;
-
-	float sensitivity = 0.1f;
-	xoffset *= sensitivity;
-	yoffset *= sensitivity;
-
-	cameraYaw += xoffset;
-	cameraPitch += yoffset;
-
-	// Prevent camera from flipping over
-	if (cameraPitch > 89.0f) cameraPitch = 89.0f;
-	if (cameraPitch < -89.0f) cameraPitch = -89.0f;
-
-	GLfloat front[3];
-	front[0] = cos(cameraYaw * M_PI / 180) * cos(cameraPitch * M_PI / 180);
-	front[1] = sin(cameraPitch * M_PI / 180);
-	front[2] = sin(cameraYaw * M_PI / 180) * cos(cameraPitch * M_PI / 180);
-	for (int i = 0; i < 3; ++i)
-		cameraFront[i] = front[i];
 }
 
 // Drawing routine.
@@ -315,19 +226,15 @@ void drawScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+
 	glLoadIdentity();
+
 	eye(R, theta, phi);
 	//gluPerspective(cameraFoV, 800.0f / 600.0f, 0.1f, 100.0f);
 	//gluLookAt(cameraPosition[0], cameraPosition[1], cameraPosition[2], cameraPosition[0] + cameraFront[0], cameraPosition[1] + cameraFront[1], cameraPosition[2] + cameraFront[2], cameraUp[0], cameraUp[1], cameraUp[2]);
 	//gluLookAt(0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-
-	// Rotate scene.
-	glRotatef(Zangle, 0.0, 0.0, 1.0);
-	glRotatef(Yangle, 0.0, 1.0, 0.0);
-	glRotatef(Xangle, 1.0, 0.0, 0.0);
 
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
@@ -370,22 +277,6 @@ void resize(int w, int h)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-// Keyboard input processing routine.
-
-// Function to handle keyboard input
-
-// Keyboard input callback
-/*
-void keyInput(unsigned char key, int x, int y)
-{
-	if (key == 'w')
-		for (int i = 0; i < 3; ++i)
-			cameraPosition[i] += cameraSpeed * cameraFront[i];
-	if (key == 's')
-		for (int i = 0; i < 3; ++i)
-			cameraPosition[i] -= cameraSpeed * cameraFront[i];
-}
-*/
 // Function to handle keyboard input
 void keyInput(unsigned char key, int x, int y)
 {
@@ -423,49 +314,6 @@ void keyInput(unsigned char key, int x, int y)
 	}
 }
 
-/*
-void keyInput(unsigned char key, int x, int y)
-{
-	switch (key)
-	{
-	case 27:
-		exit(0);
-		break;
-	case 'x':
-		Xangle += 5.0;
-		if (Xangle > 360.0) Xangle -= 360.0;
-		glutPostRedisplay();
-		break;
-	case 'X':
-		Xangle -= 5.0;
-		if (Xangle < 0.0) Xangle += 360.0;
-		glutPostRedisplay();
-		break;
-	case 'y':
-		Yangle += 5.0;
-		if (Yangle > 360.0) Yangle -= 360.0;
-		glutPostRedisplay();
-		break;
-	case 'Y':
-		Yangle -= 5.0;
-		if (Yangle < 0.0) Yangle += 360.0;
-		glutPostRedisplay();
-		break;
-	case 'z':
-		Zangle += 5.0;
-		if (Zangle > 360.0) Zangle -= 360.0;
-		glutPostRedisplay();
-		break;
-	case 'Z':
-		Zangle -= 5.0;
-		if (Zangle < 0.0) Zangle += 360.0;
-		glutPostRedisplay();
-		break;
-	default:
-		break;
-	}
-}
-*/
 
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
@@ -491,11 +339,9 @@ int main(int argc, char** argv)
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("OBJmodelViewer.cpp");
 	glutDisplayFunc(drawScene);
-	//glutPassiveMotionFunc(mouse_callback);
 	glutReshapeFunc(resize);
 	glutKeyboardFunc(keyInput);
 
-	// Enable depth testing
 
 	glewExperimental = GL_TRUE;
 	glewInit();
